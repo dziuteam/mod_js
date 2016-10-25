131cc64e39a19fbbc46fb5c66b2d0e376a2ebaae
Przemek Pamirski <Przemek@MacBook-Air-Ola.local>
2016-10-25 21:08:04 +0200

    Dodałem zapytanie o zmienne i alert pokazujacy wynik na pole trojkata

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+var a = prompt('Podaj podstawę a:');
+var h = prompt('Podaj wysokość h:');
+var triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
